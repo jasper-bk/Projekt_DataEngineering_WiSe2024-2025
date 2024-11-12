bdeecffa65e660c2c5349a6085f8af240ff1e149
Jasper <jasper.bremenkamp@protonmail.com>
2024-11-12 21:13:31 +0100

    PostgreSQL eingebunden. To-Do: Tabellen automatisch anlegen

--- app/custom_microservices_build/status_information_microservice/src/main.py
+++ app/custom_microservices_build/status_information_microservice/src/main.py
@@ -8,15 +8,15 @@ from datetime import datetime
 from time import sleep
 import random
 
-# Kafka and PostgreSQL configuration
+# Kafka and PostgreSQL configuration. Loaded from environment variables. Will be defaulted, if null
 KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'localhost:9094')
 TOPIC = os.getenv('KAFKA_TOPIC', 'inbound_checkout')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
-POSTGRES_DB = os.getenv('POSTGRES_DB', 'my_database')
-POSTGRES_USER = os.getenv('POSTGRES_USER', 'my_user')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'my_password')
-DBLESS_MODE = os.getenv('DBLESS_MODE', 'true').lower() == 'true'
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'postgres')
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'application-user')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'no-secret-default-password')
+DBLESS_MODE = os.getenv('DBLESS_MODE', 'true').lower() == 'false'
 
 # Connect to PostgreSQL (if not in DBLESS_MODE)
 def connect_to_postgres():
